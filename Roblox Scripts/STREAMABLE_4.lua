b02dcf35ddd6d47a9429f7aa76fb5e904d20ddff
shezan <97002070+shezan78@users.noreply.github.com>
2023-01-13 22:10:00 -0500

    Update STREAMABLE_4.lua

--- Roblox Scripts/STREAMABLE_4.lua
+++ Roblox Scripts/STREAMABLE_4.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hharu02/ssadsdasd/main/asdas"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/scripts-for-roblox/main/as8dasdiaysdhasdasdah9asdhasudhasdasdasdasdasdsdasdasdasdiuzigycygudsfiysdykgusdfhtdrgyhrfyghrdtihurfygknjlkuukgyiigydfsgsdfusdfyugsdygyudsygudfsuygdfdfsuugugfukgydfugkydfsugyfdsusdfkygugyugksdkugydkugyfsgykudfsgkyudfsugsdfugsdfugugkydsgyusdfgyusdfgkyudfsgkyusdfukgyfdsugysdfyfsdugkdfsugkydfsgyudfsygkudfskgyudfsygufdskysdfyugu2.lua"))()
 DaHoodSettings.Prediction = 0.135
 Aiming.TargetPart = {"Head", "LeftHand", "RightHand", "LeftLowerArm", "RightLowerArm", "LeftUpperArm", "RightUpperArm", "LeftFoot", "LeftLowerLeg", "UpperTorso", "HumanoidRootPart", "LeftUpperLeg", "RightLowerLeg", "RightFoot", "LowerTorso"}
 Aiming.FOV = 17.4
