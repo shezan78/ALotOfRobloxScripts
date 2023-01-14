5a63198248266576597eef043e99209faf8c4e6c
shezan <97002070+shezan78@users.noreply.github.com>
2023-01-13 22:14:04 -0500

    Update STREAMABLE_4.lua

--- Roblox Scripts/STREAMABLE_4.lua
+++ Roblox Scripts/STREAMABLE_4.lua
@@ -1,3 +1,17 @@
+-- {[ Type /e cool for resolver ]} --
+--[[
+$$\      $$\                 $$\                 $$\                                 $$\                                              
+$$$\    $$$ |                $$ |                $$ |                                $$ |                                             
+$$$$\  $$$$ | $$$$$$\   $$$$$$$ | $$$$$$\        $$$$$$$\  $$\   $$\        $$$$$$$\ $$$$$$$\   $$$$$$\  $$$$$$$$\ $$$$$$\  $$$$$$$\  
+$$\$$\$$ $$ | \____$$\ $$  __$$ |$$  __$$\       $$  __$$\ $$ |  $$ |      $$  _____|$$  __$$\ $$  __$$\ \____$$  |\____$$\ $$  __$$\ 
+$$ \$$$  $$ | $$$$$$$ |$$ /  $$ |$$$$$$$$ |      $$ |  $$ |$$ |  $$ |      \$$$$$$\  $$ |  $$ |$$$$$$$$ |  $$$$ _/ $$$$$$$ |$$ |  $$ |
+$$ |\$  /$$ |$$  __$$ |$$ |  $$ |$$   ____|      $$ |  $$ |$$ |  $$ |       \____$$\ $$ |  $$ |$$   ____| $$  _/  $$  __$$ |$$ |  $$ |
+$$ | \_/ $$ |\$$$$$$$ |\$$$$$$$ |\$$$$$$$\       $$$$$$$  |\$$$$$$$ |      $$$$$$$  |$$ |  $$ |\$$$$$$$\ $$$$$$$$\\$$$$$$$ |$$ |  $$ |
+\__|     \__| \_______| \_______| \_______|      \_______/  \____$$ |      \_______/ \__|  \__| \_______|\________|\_______|\__|  \__|
+                                                           $$\   $$ |                                                                 
+                                                           \$$$$$$  |                                                                 
+                                                            \______/                                                                  
+]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/scripts-for-roblox/main/as8dasdiaysdhasdasdah9asdhasudhasdasdasdasdasdsdasdasdasdiuzigycygudsfiysdykgusdfhtdrgyhrfyghrdtihurfygknjlkuukgyiigydfsgsdfusdfyugsdygyudsygudfsuygdfdfsuugugfukgydfugkydfsugyfdsusdfkygugyugksdkugydkugyfsgykudfsgkyudfsugsdfugsdfugugkydsgyusdfgyusdfgkyudfsgkyusdfukgyfdsugysdfyfsdugkdfsugkydfsgyudfsygkudfskgyudfsygufdskysdfyugu2.lua"))()
 DaHoodSettings.Prediction = 0.135
 Aiming.TargetPart = {"Head", "LeftHand", "RightHand", "LeftLowerArm", "RightLowerArm", "LeftUpperArm", "RightUpperArm", "LeftFoot", "LeftLowerLeg", "UpperTorso", "HumanoidRootPart", "LeftUpperLeg", "RightLowerLeg", "RightFoot", "LowerTorso"}
