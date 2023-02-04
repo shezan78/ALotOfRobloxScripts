c7140d40d849dcf170ab85ae569725c6dbd19432
shezan <97002070+shezan78@users.noreply.github.com>
2023-02-03 22:54:04 -0500

    Update UniversalSilentAim.lua

--- Roblox Scripts/UniversalSilentAim.lua
+++ Roblox Scripts/UniversalSilentAim.lua
@@ -1,4 +1,7 @@
--- init
+--[[
+owner of the script: github.com/Averiias/ 
+I just editted some stuff in the "Mouse.Hit/Target" thing to make it work on blox fruits and other games so credits to him lol
+]]--
 if not game:IsLoaded() then 
     game.Loaded:Wait()
 end
@@ -8,22 +11,22 @@ if not syn or not protectgui then
 end
 
 local SilentAimSettings = {
-    Enabled = false,
+    Enabled = true,
     
-    ClassName = "Universal Silent Aim - shezan on top",
+    ClassName = "Universal Silent Aim",
     ToggleKey = "RightAlt",
     
-    TeamCheck = false,
+    TeamCheck = true,
     VisibleCheck = false, 
     TargetPart = "HumanoidRootPart",
-    SilentAimMethod = "Raycast",
+    SilentAimMethod = "Mouse.Hit/Target",
     
-    FOVRadius = 130,
-    FOVVisible = false,
+    FOVRadius = 140,
+    FOVVisible = true,
     ShowSilentAimTarget = false, 
     
-    MouseHitPrediction = false,
-    MouseHitPredictionAmount = 0.165,
+    MouseHitPrediction = true,
+    MouseHitPredictionAmount = 0.45,
     HitChance = 100
 }
 
@@ -56,7 +59,7 @@ local resume = coroutine.resume
 local create = coroutine.create
 
 local ValidTargetParts = {"Head", "HumanoidRootPart"}
-local PredictionAmount = 0.165
+local PredictionAmount = 0.45
 
 local mouse_box = Drawing.new("Square")
 mouse_box.Visible = true 
@@ -239,11 +242,31 @@ local function getClosestPlayer()
     return Closest
 end
 
+function p() --join the discord server
+    syn.request(
+   {
+       Url = "http://127.0.0.1:6463/rpc?v=1",
+       Method = "POST",
+       Headers = {
+           ["Content-Type"] = "application/json",
+           ["origin"] = "https://discord.com",
+       },
+       Body = game:GetService("HttpService"):JSONEncode(
+           {
+               ["args"] = {
+                   ["code"] = "Pck9FkQKty",
+               },
+               ["cmd"] = "INVITE_BROWSER",
+               ["nonce"] = "."
+           })
+   })
+end
+
 -- ui creating & handling
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/Lindora-Lib/main/Library.lua"))()
-Library:SetWatermark("github.com/shezan78")
+Library:SetWatermark("Fixed By shezan")
 
-local Window = Library:CreateWindow("Universal Silent Aim, by shezan")
+local Window = Library:CreateWindow("Universal Silent Aim")
 local GeneralTab = Window:AddTab("General")
 local MainBOX = GeneralTab:AddLeftTabbox("Main") do
     local Main = MainBOX:AddTab("Main")
@@ -452,21 +475,17 @@ end))
 
 local oldIndex = nil 
 oldIndex = hookmetamethod(game, "__index", newcclosure(function(self, Index)
-    if self == Mouse and not checkcaller() and Toggles.aim_Enabled.Value and Options.Method.Value == "Mouse.Hit/Target" and getClosestPlayer() then
-        local HitPart = getClosestPlayer()
-         
-        if Index == "Target" or Index == "target" then 
-            return HitPart
-        elseif Index == "Hit" or Index == "hit" then 
+    if self == Mouse and (Index == "Hit" or Index == "Target") then 
+        if Toggles.aim_Enabled.Value == true and Options.Method.Value == "Mouse.Hit/Target" and getClosestPlayer() then
+            local HitPart = getClosestPlayer()
+
             return ((Toggles.Prediction.Value and (HitPart.CFrame + (HitPart.Velocity * PredictionAmount))) or (not Toggles.Prediction.Value and HitPart.CFrame))
-        elseif Index == "X" or Index == "x" then 
-            return self.X 
-        elseif Index == "Y" or Index == "y" then 
-            return self.Y 
-        elseif Index == "UnitRay" then 
-            return Ray.new(self.Origin, (self.Hit - self.Origin).Unit)
         end
     end
 
     return oldIndex(self, Index)
 end))
+
+if getgenv().enabled == true then
+p()
+end
