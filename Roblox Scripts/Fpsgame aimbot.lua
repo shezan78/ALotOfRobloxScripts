8a5128abd3612b070bbabbb19c3f7f54e28e4359
shezan <97002070+shezan78@users.noreply.github.com>
2023-03-27 19:55:32 -0400

    Update Fpsgame aimbot.lua

--- Roblox Scripts/Fpsgame aimbot.lua
+++ Roblox Scripts/Fpsgame aimbot.lua
@@ -1,1 +1,14 @@
+--[[
+
+  _               _      __                                          _            _      ___  
+ | |             | |    / _|                                        | |          | |    |__ \ 
+ | |__   ___  ___| |_  | |_ _ __  ___    __ _  __ _ _ __ ___   ___  | | ___   ___| | __    ) |
+ | '_ \ / _ \/ __| __| |  _| '_ \/ __|  / _` |/ _` | '_ ` _ \ / _ \ | |/ _ \ / __| |/ /   / / 
+ | |_) |  __/\__ \ |_  | | | |_) \__ \ | (_| | (_| | | | | | |  __/ | | (_) | (__|   <   |_|  
+ |_.__/ \___||___/\__| |_| | .__/|___/  \__, |\__,_|_| |_| |_|\___| |_|\___/ \___|_|\_\  (_)  
+                           | |           __/ |                                                
+                           |_|          |___/                                                 
+
+]]
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/scripts-for-roblox/main/PROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUAPROTECTEDLUA.lua"))()
